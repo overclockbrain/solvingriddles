c8e15ece9fc84790f012e5b1d901218925a818bd
hot brain <63366646+overclockbrain@users.noreply.github.com>
2025-12-01 11:21:29 +0900

    first commit

---
+++ src/main/java/com/example/solvingriddles/SolvingriddlesApplication.java
@@ -0,0 +1,13 @@
+package com.example.solvingriddles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingriddlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvingriddlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solvingriddles/SolvingriddlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solvingriddles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvingriddlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
